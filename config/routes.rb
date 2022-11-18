9bd373c1f9c50c60eeab001539c39a05e0999788
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-11-18 20:59:45 +0500

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-  get 'users/new'
+  # get 'users/new'
   # get 'static_pages/home'
-
+  
   root 'static_pages#home'
   get '/help',    to: 'static_pages#help'
   get '/about',   to: 'static_pages#about'
   get '/contact', to: 'static_pages#contact'
- 
+  get '/signup',  to: 'users#new'
+  
 end
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -2,7 +2,8 @@ require "test_helper"
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
   test "should get new" do
-    get users_new_url
+    # get users_new_url
+    get signup_path
     assert_response :success
   end
 end
