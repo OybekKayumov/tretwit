73a6ba39c22bd2ee365db78eedcc5a06eb6b0cf3
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-11-19 12:33:39 +0500

    Add user and email validations

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -11,5 +11,25 @@ class UserTest < ActiveSupport::TestCase
 
   test "should be valid" do
     assert @user.valid?
-  end  
+  end
+
+  test "name should be present" do
+    @user.name = "    "
+    assert_not @user.valid?
+  end
+
+  test "email should be present" do
+    @user.email = "    "
+    assert_not @user.valid?
+  end
+
+  test "name should not be too long" do
+    @user.name = "a" * 51
+    assert_not @user.valid?
+  end
+
+  test "email should not be too long" do
+    @user.email = "a" * 244 + "@example.com"
+    assert_not @user.valid?
+  end
 end
