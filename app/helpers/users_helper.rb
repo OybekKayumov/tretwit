1c50afbf424b60bad0ee1686144e0bee11c369cc
aboongm <aboongm@yahoo.com>
2022-11-21 16:41:33 +0530

    Update the gravatar image size

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,8 +1,7 @@
 module UsersHelper
-    def gravatar_for(user, size: 80)
+    def gravatar_for(user, size: 100)
         gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
-        gravatar_url = "https://secure.gravatar.com/avatar/#
-        {gravatar_id}?s=#{size}"
+        gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
         image_tag(gravatar_url, alt: user.name, class: "gravatar")
-    end   
+    end
 end
