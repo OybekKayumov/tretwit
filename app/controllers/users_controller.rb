38690e2c7c8b0563c3d713024ade079882d2e9d7
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-11-21 16:28:08 +0500

    Add new user form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,10 @@
 class UsersController < ApplicationController
-  def new
-    debugger
-  end
 
   def show
     @user = User.find(params[:id])
+  end
+  
+  def new
+    @user = User.new
   end
 end
