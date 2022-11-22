60ab5f647d9cb9269af822c6344ed3d24cab4cb0
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-11-22 14:09:55 +0500

    Add error messages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,6 +12,8 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       # Handle a successful save.
+      flash[:success] = "Welcome to the Tretwit App!"
+      redirect_to @user
     else
       render 'new', status: :unprocessable_entity
     end
