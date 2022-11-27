68e2aeb157fe3954b9be17235b76fd7eaf7edcf6
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-11-27 19:31:04 +0500

    Add login with valid info

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -4,6 +4,10 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
+  def setup
+    @user = users(:michael)
+  end
+  
   test "login with invalid information" do
     get login_path
     assert_template 'sessions/new'
@@ -12,5 +16,15 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert_not flash.empty?
     get root_path
     assert flash.empty?
+  end
+
+  test "login with valid information followed by logout" do
+    post login_path, params: { session: { email: @user.email, password: 'password' }}
+    assert_redirected_to @user
+    follow_redirect!
+    assert_template 'users/show'
+    assert_select "a[href=?]", login_path, count: 0
+    assert_select "a[href=?]", logout_path
+    assert_select "a[href=?]", user_path(@user)
   end
 end
