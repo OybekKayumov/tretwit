c7cd4eddc72db7ef02f4f378346635829bcb71c1
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-11-28 19:56:13 +0500

    Add remember_digest to users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_19_094205) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_28_145256) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,6 +20,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_19_094205) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.string "remember_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
