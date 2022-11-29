138a16de2d61947f2e91b6606f7afed020da0511
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-11-29 20:14:45 +0500

    Add integration test for user logout

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -28,11 +28,13 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert_template 'users/show'
     assert_select "a[href=?]", login_path, count: 0
     assert_select "a[href=?]", logout_path
-    assert_select "a[href=?]", user_path(@user)
+    assert_select "a[href=?]", user_path(@user) 
     delete logout_path
-    assert_not is_logged_in?
     assert_response :see_other
+    assert_not is_logged_in?
     assert_redirected_to root_url
+    # Simulate a user clicking logout in a second window.
+    delete logout_path
     follow_redirect!
     assert_select "a[href=?]", login_path
     assert_select "a[href=?]", logout_path,      count: 0
@@ -49,5 +51,10 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     follow_redirect!
     assert_template 'users/show'
     assert is_logged_in?
+  end
+
+  test "should still work after logout in second window" do
+    delete logout_path
+    assert_redirected_to root_url
   end
 end
