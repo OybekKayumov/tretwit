44be94fbb735bc9b6e40b69f54bb26336bbc5015
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-12-01 20:28:16 +0500

    Add correct_user to users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :logged_in_user, only: [:edit, :update]
+  before_action :correct_user, only: [:edit, :update]
 
   def show
     @user = User.find(params[:id])
@@ -52,5 +53,11 @@ class UsersController < ApplicationController
       flash[:danger] = "Please log in."
       redirect_to login_url, status: :see_other
     end
+  end
+
+  # Confirms the correct user.
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_url, status: :see_other) unless @user == current_user
   end
 end
