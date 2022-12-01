b9e8e314fc626c920b6f98bc7fb2c70c4f5d03c6
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-12-01 20:49:56 +0500

    Update controllers and helper

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -49,7 +49,7 @@ class UsersController < ApplicationController
   # Confirms a logged-in user.
   def logged_in_user
     unless logged_in?
-    
+      store_location
       flash[:danger] = "Please log in."
       redirect_to login_url, status: :see_other
     end
