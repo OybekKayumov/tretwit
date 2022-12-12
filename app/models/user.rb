d1f40001ea9938a1ae07739e14bcf5be73dc6fce
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-12-12 18:22:42 +0500

    Add accosiations to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,11 @@ class User < ApplicationRecord
   has_many :active_relationships, class_name:  "Relationship",
                                   foreign_key: "follower_id",
                                   dependent:   :destroy
+  has_many :following, through: :active_relationships, source: :followed
+  has_many :passive_relationships, class_name:  "Relationship",
+                                    foreign_key: "followed_id",
+                                    dependent:   :destroy
+  has_many :followers, through: :passive_relationships, source: :follower
 
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -89,6 +94,21 @@ class User < ApplicationRecord
     Micropost.where("user_id = ?", id)
   end
 
+  # Follows a user.
+  def follow(other_user)
+    following << other_user unless self == other_user
+  end
+
+  # Unfollows a user.
+  def unfollow(other_user)
+    following.delete(other_user)
+  end
+
+  # returns true if the current user is following the other user
+  def following?(other_user)
+    following.include?(other_user)
+  end
+  
   private
 
     # Converts email to all lower-case.
