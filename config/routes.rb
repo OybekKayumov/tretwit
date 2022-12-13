bdff26d43efffcdfafbd1c16bad72d187d1b9549
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-12-13 19:11:33 +0500

    Add seeds and routes

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,12 @@ Rails.application.routes.draw do
   post '/login',    to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
 
-  resources :users
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
+  
   resources :account_activations, only: [:edit]
   resources :password_resets,     only: [:new, :create, :edit, :update]
   resources :microposts,          only: [:create, :destroy]
--- db/seeds.rb
+++ db/seeds.rb
@@ -38,3 +38,11 @@ users = User.order(:created_at).take(6)
   content = Faker::Lorem.sentence(word_count: 5)
   users.each { |user| user.microposts.create!(content: content) }
 end
+
+# generate following relationships
+users = User.all
+user = users.first
+following = users[2..20]
+followers = users[3..25]
+following.each { |followed| user.follow(followed) }
+followers.each { |follower| follower.follow(user) }
